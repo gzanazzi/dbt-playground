2e0d1f4f6d5d5928997252529c97e5fc5f125cd3
gzanazzi <gabriele.zanazzi@sdggroup.com>
2023-01-20 16:39:48 +0100

    Add sources and tests

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,3 +1,12 @@
+/* select
+	id as payment_id,
+	orderid as order_id,
+	paymentmethod as payment_method,
+	status,
+	amount / 100 as amount,
+	created as created_at
+from raw.stripe.payment */
+
 select
 	id as payment_id,
 	orderid as order_id,
@@ -5,4 +14,4 @@ select
 	status,
 	amount / 100 as amount,
 	created as created_at
-from raw.stripe.payment
+from {{ source('stripe', 'payment') }}
