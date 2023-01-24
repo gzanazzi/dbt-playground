2dcff1a752f0606e1d58db5f732d712d317f0276
gzanazzi <gabriele.zanazzi@sdggroup.com>
2023-01-24 11:14:58 +0100

    Advanced testing: second trial

--- tests/average_dollars_spent_greater_than_one.sql
+++ tests/average_dollars_spent_greater_than_one.sql
@@ -1,4 +1,4 @@
-{% test average_dollars_spent_greater_than_one( model, column_name, group_by_column) %}
+/* {% test average_dollars_spent_greater_than_one( model, column_name, group_by_column) %}
 
 select 
     {{ group_by_column }},
@@ -9,4 +9,4 @@ group by 1
 having average_amount < 1
 
 
-{% endtest %}
+{% endtest %} */
